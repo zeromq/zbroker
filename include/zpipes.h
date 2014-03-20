f7e73be4dd95cd00372daa4ee4eb2618382899f0
Pieter Hintjens <ph@imatix.com>
2014-03-20 11:05:01 +0200

    Problem: using custom LGPLv3+ license
    The ZeroMQ license (LGPLv3 + static link exception) works well except
    for being a custom license that causes issues with some legal teams.
    To all extents the MPLv2 license now offers the same model of sharing
    patches while allowing static linking in any application.
    
    This patch applies the MPLv2 to all code in this project.

--- include/zpipes.h
+++ include/zpipes.h
@@ -1,7 +1,9 @@
 /*  =========================================================================
     zpipes - start/stop zpipes file sharing service
 
-    Copyright (c) tbd
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
     =========================================================================*/
 
 #ifndef __ZPIPES_H_INCLUDED__
--- src/zpipes_msg.c
+++ src/zpipes_msg.c
@@ -13,21 +13,12 @@
     * The code generation script that built this file: zproto_codec_c
     ************************************************************************
     
-    Copyright contributors as noted in the AUTHORS file.                    
-    This file is part of zbroker, the ZeroMQ broker project.                
-                                                                            
-    This is free software; you can redistribute it and/or modify it under   
-    the terms of the GNU Lesser General Public License as published by the  
-    Free Software Foundation; either version 3 of the License, or (at your  
-    option) any later version.                                              
-                                                                            
-    This software is distributed in the hope that it will be useful, but    
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABIL-
-    ITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General     
-    Public License for more details.                                        
-                                                                            
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.    
+    Copyright contributors as noted in the AUTHORS file.               
+    This file is part of zbroker, the ZeroMQ broker project.           
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
     =========================================================================
 */
 

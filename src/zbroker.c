b58bf730dc87965e8210b2936c45af8402f9ef03
Pieter Hintjens <ph@imatix.com>
2014-05-05 11:53:44 +0200

    Implement process lockfile and run user/group
    - sets effective UID, typically root
    - grabs write lock on server/lockfile (e.g. /var/run/zbroker.pid)
    - dumps PID into lockfile
    - sets group and/or user from server/group, server/user
    - else sets back to real UID
    
    Note, does not work using normal 'make' as zbroker isn't the real binary.
    Here is how I build/test:
    
        MAIN=zbroker
        sudo rm $MAIN
        gcc -g -o $MAIN \
            $MAIN.c \
            zpipes_msg.c zpipes_client.c zpipes_server.c \
            -lczmq -lzmq -lzyre -luuid -lsodium
        test $? -ne 0 && exit
    
        sudo chown root $MAIN
        sudo chgrp root $MAIN
        sudo chmod +s $MAIN
        ./$MAIN
    
    Note example of zbroker.cfg:
    
        server
            timeout = 10        #   Client connection timeout
            background = 0      #   Run as background process
            workdir = .         #   Working directory for daemon
            animate = 1         #   Do we animate or not?
            user = ph           #   Run as this user
            group = ph          #   Run as this group
            lockfile = /var/run/zbroker.pid
    
    Fixes #52
    Fixes #53

--- src/zbroker.c
+++ src/zbroker.c
@@ -19,6 +19,10 @@
 "This Software is provided under the MPLv2 License on an \"as is\" basis,\n" \
 "without warranty of any kind, either expressed, implied, or statutory.\n"
 
+//  Helpers that should move into CZMQ/zsys
+static int
+s_zsys_run_as (const char *lockfile, const char *group, const char *user);
+
 int main (int argc, char *argv [])
 {
     puts (PRODUCT);
@@ -36,31 +40,122 @@ int main (int argc, char *argv [])
         config_file = argv [1];
     
     //  Load config file for our own use here
-    zclock_log ("I: starting zpipes server using config in '%s'", config_file);
+    zclock_log ("I: starting zpipes broker using config in '%s'", config_file);
     zconfig_t *config = zconfig_load (config_file);
     if (config) {
-        //  Do we want to run server in the background?
+        //  Switch to user/group to run process under, if any
+        if (s_zsys_run_as (
+            zconfig_resolve (config, "server/lockfile", NULL),
+            zconfig_resolve (config, "server/group", NULL),
+            zconfig_resolve (config, "server/user", NULL)))
+            return -1;
+
+        //  Do we want to run broker in the background?
         int as_daemon = atoi (zconfig_resolve (config, "server/background", "0"));
         const char *workdir = zconfig_resolve (config, "server/workdir", ".");
         if (as_daemon) {
             zclock_log ("I: broker switching to background process...");
-            zsys_daemonize (workdir);
+            if (zsys_daemonize (workdir))
+                return -1;
         }
         zconfig_destroy (&config);
     }
     else {
-        zclock_log ("E: cannot load config file '%s'\\n", config_file);
+        zclock_log ("E: cannot load config file '%s'\n", config_file);
         return 1;
     }
     zpipes_server_t *zpipes_server = zpipes_server_new ();
     zpipes_server_configure (zpipes_server, config_file);
-    
+
     //  Wait until process is interrupted
     while (!zctx_interrupted)
         zclock_sleep (1000);
+
     puts ("interrupted");
 
     //  Shutdown all services
     zpipes_server_destroy (&zpipes_server);
+    return 0;
+}
+
+
+//  --------------------------------------------------------------------------
+//  Set-up the process to run as a specified group and user.
+//  First, if lockfile is specified, tries to grab a write lock on that
+//  file, and if successful, dumps the process ID into the file. This
+//  prevents two instances of the process from starting up by accident.
+//  Second, if group is specified, switches to using that group. Third,
+//  if user is specified, switches to using that group. If user is null,
+//  switches to the real user ID after dropping the PID into the lockfile.
+//  Returns 0 on success, -1 on any error.
+
+static int
+s_zsys_run_as (const char *lockfile, const char *group, const char *user)
+{
+    //  Switch to effective user ID (who owns executable); for
+    //  system services this should be root, so that we can write
+    //  the PID file into e.g. /var/run/
+    if (seteuid (geteuid ())) {
+        zclock_log ("E: cannot set effective user id: %s\n",
+                    strerror (errno));
+        return -1;
+    }
+    if (lockfile) {
+        //  We enforce a lock on the lockfile, if specified, so that
+        //  only one copy of the process can run at once.
+        int handle = open (lockfile, O_RDWR | O_CREAT, 0640);
+        if (handle < 0) {
+            zclock_log ("E: cannot open lockfile '%s': %s\n",
+                        lockfile, strerror (errno));
+            return -1;          
+        }
+        else {
+            struct flock filelock;
+            filelock.l_type   = F_WRLCK;    //  F_RDLCK, F_WRLCK, F_UNLCK
+            filelock.l_whence = SEEK_SET;   //  SEEK_SET, SEEK_CUR, SEEK_END
+            filelock.l_start  = 0;          //  Offset from l_whence
+            filelock.l_len    = 0;          //  length, 0 = to EOF
+            filelock.l_pid    = getpid ();
+            if (fcntl (handle, F_SETLK, &filelock)) {
+                zclock_log ("E: cannot get lock: %s\n", strerror (errno));
+                return -1;              
+            }
+        }
+        //   We record the broker's process id in the lock file
+        char pid_buffer [10];
+        snprintf (pid_buffer, sizeof (pid_buffer), "%6d\n", getpid ());
+        if (write (handle, pid_buffer, strlen (pid_buffer)) != strlen (pid_buffer)) {
+            zclock_log ("E: cannot write to lockfile: %s\n",
+                        strerror (errno));
+            return -1;
+        }
+        close (handle);
+    }
+    if (group) {
+        zclock_log ("I: broker running under group '%s'", group);
+        struct group *grpbuf = NULL;
+        grpbuf = getgrnam (group);
+        if (grpbuf == NULL || setgid (grpbuf->gr_gid)) {
+            zclock_log ("E: could not switch group: %s", strerror (errno));
+            return -1;
+        }
+    }
+    if (user) {
+        zclock_log ("I: broker running under user '%s'", user);
+        struct passwd *pwdbuf = NULL;
+        pwdbuf = getpwnam (user);
+        if (pwdbuf == NULL || setuid (pwdbuf->pw_uid)) {
+            zclock_log ("E: could not switch user: %s", strerror (errno));
+            return -1;
+        }
+    }
+    else {
+        //  Switch back to real user ID (who started process)
+        if (setuid (getuid ())) {
+            zclock_log ("E: cannot set real user id: %s\n",
+                        strerror (errno));
+            return -1;
+        }
+    }
     return 0;
 }

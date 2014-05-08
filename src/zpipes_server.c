98f5997b9127caaa35554abd2073ba19b2daffcd
Pieter Hintjens <ph@imatix.com>
2014-05-08 18:29:04 +0200

    Problem: handshake killed 3rd innocent node
    When a node received a "HAVE WRITER" and then "HAVE READER" from
    the cluster, it would falsely try to become a pipe writer. This
    caused an assertion failure on zpipes_server.c:262 as the code
    caught this impossible situation.
    
    Solution: when seeing two peers handshake the same pipe, drop the
    pipe entirely.
    
    Fixes #82.
    
    Note: I didn't set up a reproducible test case so I'm not 100%
    sure the fix is accurate.

--- src/zpipes_server.c
+++ src/zpipes_server.c
@@ -242,6 +242,13 @@ static int
 pipe_attach_remote_reader (pipe_t *self, const char *remote, bool unicast)
 {
     assert (self);
+    if (self->writer == REMOTE_NODE) {
+        //  We're witnessing two nodes chatting, so we can drop the pipe
+        //  and forget all about it
+        pipe_destroy (&self);
+        return 0;
+    }
+    else
     if (self->reader == NULL) {
         //  This is how we indicate a remote reader
         self->reader = REMOTE_NODE;
@@ -260,7 +267,6 @@ pipe_attach_remote_reader (pipe_t *self, const char *remote, bool unicast)
         }
         //  Writer must be local at this stage; wake it up so it can
         //  ship off its waiting data
-        assert (self->writer != REMOTE_NODE);
         engine_send_event (self->writer, have_reader_event);
         return 0;
     }
@@ -313,6 +319,13 @@ static int
 pipe_attach_remote_writer (pipe_t *self, const char *remote, bool unicast)
 {
     assert (self);
+    if (self->reader == REMOTE_NODE) {
+        //  We're witnessing two nodes chatting, so we can drop the pipe
+        //  and forget all about it
+        pipe_destroy (&self);
+        return 0;
+    }
+    else
     if (self->writer == NULL) {
         //  This is how we indicate a remote writer
         self->writer = REMOTE_NODE;

de7fba970807b913f457ead18f1b0a41a2fe77c4
Pieter Hintjens <ph@imatix.com>
2014-05-24 15:13:32 +0200

    Config file zyre/name specifies node name
    Also: zyre/interface specifies node interface to use
    
    Fixes #81
    Fixes #89

--- src/zpipes_test_cluster.c
+++ src/zpipes_test_cluster.c
@@ -22,32 +22,46 @@ s_wait (char *message)
 int main (void)
 {
     char *animate = "0";
+
+    zactor_t *green = zactor_new (zpipes_server, NULL);
+    //  BIND message always replies with port number
+    zstr_sendx (green, "BIND", "ipc://@/zpipes/green", NULL);
+    char *reply = zstr_recv (green);
+    assert (streq (reply, "0"));
+    free (reply);
     
-    zactor_t *hosta = zactor_new (zpipes_server, NULL);
-    zstr_sendx (hosta, "BIND", "ipc://@/zpipes/hosta", NULL);
-    zstr_sendx (hosta, "SET", "server/animate", animate, NULL);
-    zstr_sendx (hosta, "SET", "zyre/interval", "100", NULL);
-    zstr_sendx (hosta, "SET", "zyre/nodeid", "hosta", NULL);
-    zstr_sendx (hosta, "JOIN CLUSTER", NULL);
-    char *reply = zstr_recv (hosta);
+    zstr_sendx (green, "SET", "server/animate", animate, NULL);
+    zstr_sendx (green, "SET", "zyre/interval", "100", NULL);
+    zstr_sendx (green, "SET", "zyre/nodeid", "green", NULL);
+    
+    //  JOIN CLUSTER message always replies with OK/SNAFU
+    zstr_sendx (green, "JOIN CLUSTER", NULL);
+    reply = zstr_recv (green);
 
     //  If the machine has no usable broadcast interface, the JOIN CLUSTER
     //  command will fail, and then there's no point in continuing...
     if (strneq (reply, "OK")) {
         zclock_log ("W: skipping test, no UDP discovery");
         free (reply);
-        zactor_destroy (&hosta);
+        zactor_destroy (&green);
         return 0;
     }
     free (reply);
 
-    zactor_t *hostb = zactor_new (zpipes_server, NULL);
-    zstr_sendx (hostb, "BIND", "ipc://@/zpipes/hostb", NULL);
-    zstr_sendx (hostb, "SET", "server/animate", animate, NULL);
-    zstr_sendx (hostb, "SET", "zyre/interval", "100", NULL);
-    zstr_sendx (hostb, "SET", "zyre/nodeid", "hostb", NULL);
-    zstr_sendx (hostb, "JOIN CLUSTER", NULL);
-    reply = zstr_recv (hostb);
+    zactor_t *orange = zactor_new (zpipes_server, NULL);
+    //  BIND message always replies with port number
+    zstr_sendx (orange, "BIND", "ipc://@/zpipes/orange", NULL);
+    reply = zstr_recv (orange);
+    assert (streq (reply, "0"));
+    free (reply);
+    
+    zstr_sendx (orange, "SET", "server/animate", animate, NULL);
+    zstr_sendx (orange, "SET", "zyre/interval", "100", NULL);
+    zstr_sendx (orange, "SET", "zyre/nodeid", "orange", NULL);
+    
+    //  JOIN CLUSTER message always replies with OK/SNAFU
+    zstr_sendx (orange, "JOIN CLUSTER", NULL);
+    reply = zstr_recv (orange);
     assert (streq (reply, "OK"));
     free (reply);
     
@@ -59,10 +73,10 @@ int main (void)
 
     //  Test 1 - simple read-write
     s_wait ("Open writer");
-    zpipes_client_t *writer = zpipes_client_new ("hostb", ">test pipe");
+    zpipes_client_t *writer = zpipes_client_new ("orange", ">test pipe");
 
     s_wait ("Open reader");
-    zpipes_client_t *reader = zpipes_client_new ("hosta", "test pipe");
+    zpipes_client_t *reader = zpipes_client_new ("green", "test pipe");
 
     //  Expect timeout error, EAGAIN
     s_wait ("Read impatiently");
@@ -116,10 +130,10 @@ int main (void)
     
     //  Test 2 - pipe reuse
     s_wait ("Open reader");
-    reader = zpipes_client_new ("hosta", "test pipe 2");
+    reader = zpipes_client_new ("green", "test pipe 2");
 
     s_wait ("Open writer");
-    writer = zpipes_client_new ("hostb", ">test pipe 2");
+    writer = zpipes_client_new ("orange", ">test pipe 2");
 
     s_wait ("Close reader");
     zpipes_client_destroy (&reader);
@@ -128,10 +142,10 @@ int main (void)
     zpipes_client_destroy (&writer);
 
     s_wait ("Open reader reusing pipe name");
-    reader = zpipes_client_new ("hosta", "test pipe 2");
+    reader = zpipes_client_new ("green", "test pipe 2");
     zpipes_client_destroy (&reader);
 
-    zactor_destroy (&hosta);
-    zactor_destroy (&hostb);
+    zactor_destroy (&green);
+    zactor_destroy (&orange);
     return 0;
 }
